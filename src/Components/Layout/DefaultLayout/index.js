bfdfd1aab106c169fd1287ca14840d9d54ba8eca
Than Van Ky <thanky4444.com>
2024-12-20 09:00:43 +0700

    set Layout

--- src/Components/Layout/DefaultLayout/index.js
+++ src/Components/Layout/DefaultLayout/index.js
@@ -1,17 +1,21 @@
+
+import classNames from 'classnames/bind';
 import Header from '~/Components/Layout/Components/Header'
 import Sidebar from './Sidebar'
+import styles from './DefaultLayout.module.scss'
 
+
+const cx = classNames.bind(styles)
 
 function DefaultLayout({ children }) {
     return (
-        <div>
+        <div className={cx('wrapper')}>
             <Header />
-            <div className='container'>
+            <div className={cx('container')}>
                 <Sidebar />
-                <div className='content'>
+                <div className={cx('content')}>
                     {children}
                 </div>
-
             </div>
         </div>
     )
