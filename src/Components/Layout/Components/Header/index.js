8ddec1a8226412fc5ef777f66f197c79db61501a
Than Van Ky <thanky4444.com>
2024-12-20 10:49:41 +0700

    HeaderUI #1

--- src/Components/Layout/Components/Header/index.js
+++ src/Components/Layout/Components/Header/index.js
@@ -1,15 +1,29 @@
 
 import classNames from 'classnames/bind';
 import styles from './Header.module.scss'
-
-
-
+import images from '~/assets/images'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
 const cx = classNames.bind(styles)
 
 function Header() {
     return (
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
+                <img src={images.logo} alt='Tiktok'></img>
+
+                <div className={cx('search')}>
+                    <input placeholder='Search account and video' spellCheck={false} />
+                    <button className={cx('clear')}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+
+                    </button>
+                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+                <div className={cx('actions')}>hell</div>
             </div>
         </header>
     );
