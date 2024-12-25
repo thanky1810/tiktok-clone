3816bbd132c3a6c5b4e95384ca891c8dddb089fe
Than Van Ky <thanky4444.com>
2024-12-25 12:07:42 +0700

    Header UI #6

--- src/Components/Popper/Menu/index.js
+++ src/Components/Popper/Menu/index.js
@@ -37,6 +37,7 @@ function Menu({ children, items = [], onChange = defaulFn }) {
         <Tippy
             interactive
             delay={[0, 700]}
+            offset={[12, 8]}
             placement='bottom-end'
             render={(attrs) => (
                 <div className={cx('menu-list')} tabIndex="-1" {...attrs}>
@@ -52,6 +53,11 @@ function Menu({ children, items = [], onChange = defaulFn }) {
                     </PopperWrapper>
                 </div>
             )}
+            onHide={() => {
+                setHistory(prev =>
+                    prev.slice(0, 1)
+                )
+            }}
         >
             {children}
         </Tippy>
