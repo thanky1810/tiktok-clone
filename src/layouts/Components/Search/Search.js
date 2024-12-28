284d12e0442ed98ddebb93ef909789d6b66a856e
Than Van Ky <thanky4444.com>
2024-12-28 11:22:50 +0700

    optimize #3

---
+++ src/layouts/Components/Search/Search.js
@@ -0,0 +1,109 @@
+import { useEffect, useState, useRef } from 'react';
+import { faCircleXmark, faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { Wrapper as PopperWrapper } from '~/components/Popper';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { SearchIcon } from '~/components/Icons';
+import HeadlessTippy from '@tippyjs/react/headless';
+
+import * as searchServices from '~/services/searchServices';
+import AccountItem from '~/components/AccountItem';
+import classNames from 'classnames/bind';
+import styles from './Search.module.scss';
+import { useDebounce } from '~/Hooks';
+
+const cx = classNames.bind(styles);
+
+function Search() {
+    const [searchValue, setSearchValue] = useState('');
+    const [searchResult, setSearchResult] = useState([]);
+    const [showResult, setShowResult] = useState(true);
+    const [loading, setLoading] = useState(false);
+
+    const inputRef = useRef();
+
+    const debounced = useDebounce(searchValue, 800);
+
+
+    const handleClear = () => {
+        setSearchValue('');
+        inputRef.current.focus();
+        setSearchResult([]);
+    };
+
+    const handleHideResult = () => {
+        setShowResult(false);
+    };
+
+    useEffect(() => {
+        if (!debounced.trim()) {
+            setSearchResult([]);
+            return;
+        }
+
+        const fetchApi = async () => {
+            setLoading(true)
+
+            const result = await searchServices.search(debounced);
+
+            setSearchResult(result)
+
+            setLoading(false)
+        }
+
+
+        fetchApi();
+    }, [debounced]);
+
+
+    const handleChange = (e) => {
+        const searchValue = e.target.value;
+        if (!searchValue.startsWith(' ')) {
+            setSearchValue(searchValue);
+        }
+    };
+
+
+
+    return (
+        <div>
+            <HeadlessTippy
+                interactive
+                visible={searchResult.length > 0 && showResult}
+                render={(attrs) => (
+                    <div className={cx('search-result')} tabIndex="-1" {...attrs}>
+                        <PopperWrapper>
+                            <h4 className={cx('search-title')}>Accounts</h4>
+                            {searchResult.map((result) => (
+                                <AccountItem key={result.id} data={result} />
+                            ))}
+                        </PopperWrapper>
+                    </div>
+                )}
+                onClickOutside={handleHideResult}
+            >
+                <div className={cx('search')}>
+                    <input
+                        ref={inputRef}
+                        value={searchValue}
+                        placeholder="Search accounts and videos"
+                        spellCheck={false}
+                        onChange={handleChange}
+                        onFocus={() => setShowResult(true)}
+                    />
+                    {!!searchValue && !loading && (
+                        <button className={cx('clear')} onClick={handleClear}>
+                            <FontAwesomeIcon icon={faCircleXmark} />
+                        </button>
+                    )}
+                    {loading && <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />}
+                    <button className={cx('search-btn')} onMouseDown={(e) => e.preventDefault()} >
+
+                        <SearchIcon />
+                    </button>
+                </div>
+            </HeadlessTippy>
+        </div>
+    );
+}
+
+export default Search;
---
+++ src/routes/routes.js
@@ -0,0 +1,28 @@
+
+import config from '~/config'
+//HeaderOnly
+import { HeaderOnly } from '~/layouts'
+
+
+//Pages
+import Home from '~/pages/Home'
+import Following from '~/pages/Following'
+import Profile from '~/pages/Profile'
+import Upload from '~/pages/Upload'
+import Search from '~/pages/Search'
+// for features needn't login still use it
+const publicRoutes = [
+    { path: config.routes.home, component: Home },
+    { path: config.routes.following, component: Following },
+    { path: config.routes.upload, component: Upload, layout: HeaderOnly },
+    { path: config.routes.search, component: Search, layout: null },
+    { path: config.routes.profile, component: Profile }
+];
+
+
+
+
+// for features have to login still use it
+const privateRoutes = [];
+
+export { publicRoutes, privateRoutes }
