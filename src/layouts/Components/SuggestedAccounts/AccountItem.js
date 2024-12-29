f44b7479894c8005e41e612844e3b901b2d50713
Than Van Ky <thanky4444.com>
2024-12-29 14:57:14 +0700

    Sidebar #3

--- src/layouts/Components/SuggestedAccounts/AccountItem.js
+++ src/layouts/Components/SuggestedAccounts/AccountItem.js
@@ -1,29 +1,50 @@
 import classNames from 'classnames/bind'
-import PropTypes from 'prop-types'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import Tippy from '@tippyjs/react/headless';
+import { Wrapper as PopperWrapper } from '~/components/Popper';
 
 import styles from './SuggestAccounts.module.scss'
+import AccountPreview from './AccountPreview';
 
 
 
 const cx = classNames.bind(styles)
 
+const renderPreview = (props) => {
+    return (
+        <div tabIndex='-1' {...props}>
+            <PopperWrapper>
+                <AccountPreview />
+            </PopperWrapper>
+        </div>
+    )
+}
 function AccountItem() {
     return (
-        <div className={cx('account-item')}>
-            <img
-                className={cx('avatar')}
-                src='https://i0.wp.com/spellmana.com/wp-content/uploads/2024/07/roronoa-zoro-deck-guide.webp?fit=768%2C429&amp;ssl=1' alt=''>
-            </img>
-            <div className={cx('item-info')}>
-                <p className={cx('nickname')}>
-                    <strong>thanvanky</strong>
-                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
-                </p>
-                <p className={cx('name')}>Than Van ky</p>
-            </div>
+        <div>
+            <Tippy
+                interactive
+                delay={[1000, 0]}
+                offset={[-20, 0]}
+                render={renderPreview}
+                placement='bottom'
+            >
+                <div className={cx('account-item')}>
+                    <img
+                        className={cx('avatar')}
+                        src='https://i0.wp.com/spellmana.com/wp-content/uploads/2024/07/roronoa-zoro-deck-guide.webp?fit=768%2C429&amp;ssl=1' alt=''>
+                    </img>
+                    <div className={cx('item-info')}>
+                        <p className={cx('nickname')}>
+                            <strong>thanvanky</strong>
+                            <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                        </p>
+                        <p className={cx('name')}>Than Van ky</p>
+                    </div>
 
+                </div>
+            </Tippy>
         </div>
     );
 }
